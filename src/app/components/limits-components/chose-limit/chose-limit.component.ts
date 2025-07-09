368e5f7e4d48268a63af4398953cef20d771a120
Maximilian Meyer <maximilian.meyer@maxionderon.dev>
2025-07-09 17:33:25 +0200

    Implemented: -limit input fields will be disabled if wrong value was inserted into an input field till correct value was inserted

--- src/app/components/limits-components/chose-limit/chose-limit.component.ts
+++ src/app/components/limits-components/chose-limit/chose-limit.component.ts
@@ -36,6 +36,21 @@ export class ChoseLimitComponent implements OnInit {
   @Output("limit")
   eventEmitter: EventEmitter<Limit>;
 
+  protected helperMinimum: string;
+  protected helperMinimumPercent: string;
+  protected helperMaximum: string;
+  protected helperMaximumPercent: string;
+
+  protected showHelperMinimum: boolean;
+  protected showHelperMinimumPercent: boolean;
+  protected showHelperMaximum: boolean;
+  protected showHelperMaximumPercent: boolean;
+
+  protected disabledInputMinimum: boolean;
+  protected disabledInputMinimumPercent: boolean;
+  protected disabledInputMaximum: boolean;
+  protected disabledInputMaximumPercent: boolean;
+  
   constructor() {
 
     this.itemType = ItemType.mastery;
@@ -51,7 +66,22 @@ export class ChoseLimitComponent implements OnInit {
     this.selectedMinimumInPercent = 0;
     this.selectedMaximumInPercent = 0;
 
-    this.eventEmitter = new EventEmitter<Limit>();    
+    this.eventEmitter = new EventEmitter<Limit>();
+    
+    this.helperMinimum = "";
+    this.helperMinimumPercent = "";
+    this.helperMaximum = "";
+    this.helperMaximumPercent = "";
+
+    this.showHelperMinimum = false;
+    this.showHelperMinimumPercent = false;
+    this.showHelperMaximum = false;
+    this.showHelperMaximumPercent = false;
+
+    this.disabledInputMinimum = false;
+    this.disabledInputMinimumPercent = false;
+    this.disabledInputMaximum = false;
+    this.disabledInputMaximumPercent = false;
 
   }
 
@@ -66,39 +96,148 @@ export class ChoseLimitComponent implements OnInit {
     this.selectedMinimumInPercent = Constants.getPercent(this.selectedMinimum, this.itemType);
     this.selectedMaximumInPercent = Constants.getPercent(this.selectedMaximum, this.itemType);
 
+    this.emitLimit(true);
+
   }
 
   protected changeMinimum(): void {
-    
-    this.selectedMinimumInPercent = Constants.getPercent(this.selectedMinimum, this.itemType);
-    this.emitLimit();
+
+    if( this.selectedMinimum < this.minimum || this.selectedMinimum >= this.selectedMaximum) {
+
+      this.helperMinimum = this.buildHelper(this.minimum, this.selectedMaximum);
+      this.showHelperMinimum = true;
+      this.disableInput();
+      this.emitLimit(false);
+
+    } else {
+
+      this.selectedMinimumInPercent = Constants.getPercent(this.selectedMinimum, this.itemType);
+      this.showHelperMinimum = false;
+      this.enableInput();
+      this.emitLimit(true);
+
+    }
 
   }
 
   protected changeMinimumInPercent(): void {
 
-    this.selectedMinimum = Constants.getRating(this.selectedMinimumInPercent, this.itemType);
-    this.emitLimit();
+    if( this.selectedMinimumInPercent < this.minimumInPercent || this.selectedMinimumInPercent >= this.selectedMaximumInPercent ) {
+
+      this.helperMinimumPercent = this.buildHelper(this.minimumInPercent, this.selectedMaximumInPercent);
+      this.showHelperMinimumPercent = true;
+      this.disableInput();
+      this.emitLimit(false);
+
+    } else {
+
+      this.selectedMinimum = Constants.getRating(this.selectedMinimumInPercent, this.itemType);
+      this.showHelperMinimumPercent = false;
+      this.enableInput();
+      this.emitLimit(true);
+
+    }
 
   }
 
   protected changeMaximum(): void {
 
-    this.selectedMaximumInPercent = Constants.getPercent(this.selectedMaximum, this.itemType);
-    this.emitLimit();
+    if( this.selectedMaximum <= this.selectedMinimum  || this.selectedMaximum > this.maximum ) {
+
+      this.helperMaximum = this.buildHelper(this.selectedMinimum, this.maximum);
+      this.showHelperMaximum = true;
+      this.disableInput();
+      this.emitLimit(false);
+
+    } else {
+
+      this.selectedMaximumInPercent = Constants.getPercent(this.selectedMaximum, this.itemType);
+      this.showHelperMaximum = false;
+      this.enableInput();
+      this.emitLimit(true);
+
+    }
 
   }
 
   protected changeMaximumInPercent(): void {
 
-    this.selectedMaximum = Constants.getRating(this.selectedMaximumInPercent, this.itemType);
-    this.emitLimit();
+    if( this.selectedMaximumInPercent <= this.selectedMinimumInPercent || this.selectedMaximumInPercent > this.maximumInPercent ) {
+
+      this.helperMaximumPercent = this.buildHelper(this.selectedMinimumInPercent, this.maximumInPercent)
+      this.showHelperMaximumPercent = true;
+      this.disableInput();
+      this.emitLimit(false);
+
+    } else {
+
+      this.selectedMaximum = Constants.getRating(this.selectedMaximumInPercent, this.itemType);
+      this.showHelperMaximumPercent = false;
+      this.enableInput();
+      this.emitLimit(true);
+    
+    }
 
   }
 
-  private emitLimit(): void {
+  private emitLimit(legit: boolean): void {
 
-    this.eventEmitter.emit(new Limit(this.selectedMinimum, this.itemType, this.selectedMaximum, true));
+    this.eventEmitter.emit(new Limit(this.selectedMinimum, this.itemType, this.selectedMaximum, legit));
+
+  }
+
+  private buildHelper(limitOne: number, limitTwo: number): string {
+
+    return "Value should be between " + String(limitOne) + " and " + String(limitTwo) ;
+
+  }
+
+  private disableInput(): void {
+
+    if( this.showHelperMinimum == true ) {
+
+      this.disabledInputMinimum = false;
+      this.disabledInputMinimumPercent = true;
+      this.disabledInputMaximum = true;
+      this.disabledInputMaximumPercent = true;
+
+    }
+
+    if( this.showHelperMinimumPercent == true ) {
+
+      this.disabledInputMinimum = true;
+      this.disabledInputMinimumPercent = false;
+      this.disabledInputMaximum = true;
+      this.disabledInputMaximumPercent = true;
+
+    }
+
+    if( this.showHelperMaximum == true ) {
+
+      this.disabledInputMinimum = true;
+      this.disabledInputMinimumPercent = true;
+      this.disabledInputMaximum = false;
+      this.disabledInputMaximumPercent = true;
+
+    }
+
+    if( this.showHelperMaximumPercent == true ) {
+
+      this.disabledInputMinimum = true;
+      this.disabledInputMinimumPercent = true;
+      this.disabledInputMaximum = true;
+      this.disabledInputMaximumPercent = false;
+
+    }
+
+  }
+
+  private enableInput(): void {
+
+    this.disabledInputMinimum = false;
+    this.disabledInputMinimumPercent = false;
+    this.disabledInputMaximum = false;
+    this.disabledInputMaximumPercent = false;
 
   }
 
