12c7ffab7a2df48e42902da0e561112adbf9f915
Maximilian Meyer <Maximilian.Meyer@maxionderon.dev>
2025-08-01 22:35:31 +0200

    test deploy

--- src/app/model/itemClass.ts
+++
@@ -1,7 +0,0 @@
-export enum ItemClass {
-
-    enhancement = 0,
-    augment = 1,
-    stim = 2
-
-}
--- src/app/model/modifier-type.ts
+++
@@ -1,7 +0,0 @@
-export enum ModifierType {
-
-    companionBonus = 0,
-    combatOrCarnageOrGunneryOrArsenal = 1,
-    telekineticsOrLightning= 2    
-
-}
